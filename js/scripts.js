eba37cec0b1478be1b86c786e90c57c3b67ac1f2
Hyung Jin Kim <hyungjinkim1205@gmail.com>
2023-01-09 13:41:27 -0800

    add subtract multiply and divide functions

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,17 @@
   function add(number1, number2) {
     return number1 + number2;
   }
+  function subtract(number1, number2) {
+    return number1 - number2;
+  }
+  
+  function multiply(number1, number2) {
+    return number1 * number2;
+  }
+
+  function divide(number1, number2) {
+    return number1 / number2;
+  }
 
 // user interface logic
 const number1 = parseInt(prompt("Enter a number:"));
