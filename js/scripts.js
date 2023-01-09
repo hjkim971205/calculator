36777ddab0b97db2ae26632f56573aae04a2be3f
Hyung Jin Kim <hyungjinkim1205@gmail.com>
2023-01-09 14:19:15 -0800

    change format of the result

--- js/scripts.js
+++ js/scripts.js
@@ -2,28 +2,28 @@
   function add(number1, number2) {
     const result = number1 + number2;
     const msg1 = "The sum of your numbers equals " + result + "."
-    return msg1;
+    return result;
   }
   function subtract(number1, number2) {
     const result = number1 - number2;
     const msg2 = "The difference of your numbers equals " + result + "."
-    return msg2;
+    return result;
   }
   
   function multiply(number1, number2) {
     const result = number1 * number2;
     const msg3 = "The product of your numbers equals " + result + "."
-    return msg3;
+    return result;
   }
 
   function divide(number1, number2) {
     const result = number1 / number2;
     const msg4 = "The quotient of your numbers equals " + result + "."
-    return msg4;
+    return result;
   }
 
 // user interface logic
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert(add(number1, number2) + "\n" + subtract(number1, number2) + "\n" + multiply(number1,number2) + "\n" + divide(number1,number2));
+window.alert(number1 + "+" + number2 + "=" + add(number1,number2) + "\n" + number1 + "-" + number2 + "=" +  subtract(number1, number2) + "\n" + number1 + "*" + number2 + "=" + multiply(number1,number2) + "\n" + number1 + "/" + number2 + "=" + divide(number1,number2));
